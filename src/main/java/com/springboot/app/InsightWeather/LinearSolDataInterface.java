cc3c06e426b8a672af176003920e3a156bba4abf
Joseph Key <jkey3@brookdale.com>
2020-12-30 19:31:24 -0600

    update

--- src/main/java/com/springboot/app/InsightWeather/LinearSolDataInterface.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.app.InsightWeather;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface LinearSolDataInterface {
-    String getMin(JsonNode data);
-    String getAvg(JsonNode data);
-    String getMax(JsonNode data);
-}
