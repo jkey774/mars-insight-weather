fdd97af95efbdfe6b2964923bde4d278a82a5091
Joseph Key <jkey3@brookdale.com>
2020-12-30 19:29:01 -0600

    update

--- src/main/java/com/springboot/app/InsightWeather/Sol.java
+++ src/main/java/com/springboot/app/InsightWeather/Sol.java
@@ -1,19 +1,12 @@
 package com.springboot.app.InsightWeather;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import javax.sound.sampled.Line;
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 @JsonIgnoreProperties(ignoreUnknown = true)
